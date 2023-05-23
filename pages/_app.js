de5214c9c25835bf5b5f7ab4c2399b1225155805
klarstrup <io@klarstrup.dk>
2023-05-23 20:58:23 +0200

    main README

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-import App from "next/app";
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-MyApp.getInitialProps = (appContext) => App.getInitialProps(appContext);
-
-export default MyApp;
